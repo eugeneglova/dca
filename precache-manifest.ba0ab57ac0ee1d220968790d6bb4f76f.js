621302f34a150248fcdf3eb24e20285781f86a40
Eugene Glova <eugene.glova@gmail.com>
2020-04-05 23:06:02 +0300

    Updates

---
+++ precache-manifest.ba0ab57ac0ee1d220968790d6bb4f76f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c8ac3663aa9498cb5b667b7ee115dfb",
+    "url": "/dca/index.html"
+  },
+  {
+    "revision": "4dee313793f7b93f219a",
+    "url": "/dca/static/js/2.15bfc9f0.chunk.js"
+  },
+  {
+    "revision": "380b27b1e088253ea38ef66b11c645a9",
+    "url": "/dca/static/js/2.15bfc9f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb4d3cde4b40cba9b689",
+    "url": "/dca/static/js/main.189484ea.chunk.js"
+  },
+  {
+    "revision": "15042a26a487680641e5",
+    "url": "/dca/static/js/runtime-main.e226e2bf.js"
+  }
+]);
