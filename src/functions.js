fa8601afb66c0f921183370a7398ccc8811ee8e1
Eugene Glova <eugene.glova@gmail.com>
2020-07-05 13:37:45 +0300

    Add liq diff

--- src/functions.js
+++ src/functions.js
@@ -106,11 +106,12 @@ export const getOrderColumns = (rows, settings) => [
   },
   {
     name: 'liq_price',
-    title: 'Pos Liq Price',
+    title: 'Pos Liq Price (diff)',
     getCellValue: (row) => {
-      const { price, amount } = getAvgPosition(rows, rows.indexOf(row))
+      const { price, amount, op } = getAvgPosition(rows, rows.indexOf(row))
       const liq_price = getLiqPrice(price, amount, settings.leverage, settings.min_margin)
-      return precision(liq_price)
+      const diff = amount > 0 ? op - liq_price : liq_price - op
+      return `${precision(liq_price)} (${precision(diff)})`
     },
   },
   {
